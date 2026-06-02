fe7c0dbf3f24a4baef3c9e45d3f38da012c9c167
Vadlamudy Lohini <24r11a05x4@gcet.edu.in>
2026-06-02 11:50:20 +0530

    Add files via upload

---
+++ Task1_LandingPage/script.js
@@ -0,0 +1,9 @@
+function showContact()
+{
+    var contact=document.getElementById("contact-info");
+    if(contact.style.display=="block")
+    {
+        contact.style.display="none";
+    }else{
+        contact.style.display="block";
+    }}
